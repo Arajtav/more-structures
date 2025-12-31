34f63d7d7c51de28ed2c3f13e25e06714d6d2f4b
Aräjtav <arajtav@arajtav.com>
2025-12-31 17:44:01 +0100

    feat: impl Index and IndexMut for overwriting ring buffer

--- src/overwriting_ring_buf.rs
+++ src/overwriting_ring_buf.rs
@@ -1,4 +1,7 @@
-use std::mem::MaybeUninit;
+use std::{
+    mem::MaybeUninit,
+    ops::{Index, IndexMut},
+};
 
 // L > 0
 pub struct OverwritingRingBuf<T, const L: usize> {
@@ -272,6 +275,26 @@ impl<'a, T, const L: usize> IntoIterator for &'a mut OverwritingRingBuf<T, L> {
     }
 }
 
+impl<T, const L: usize> Index<usize> for OverwritingRingBuf<T, L> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        assert!(index < self.length);
+
+        // SAFETY: the element is in 0..self.length, therefore is valid.
+        unsafe { self.inner[self.read_index(index)].assume_init_ref() }
+    }
+}
+
+impl<T, const L: usize> IndexMut<usize> for OverwritingRingBuf<T, L> {
+    fn index_mut(&mut self, index: usize) -> &mut T {
+        assert!(index < self.length);
+
+        // SAFETY: the element is in 0..self.length, therefore is valid.
+        unsafe { self.inner[self.read_index(index)].assume_init_mut() }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::OverwritingRingBuf;
@@ -370,5 +393,28 @@ mod tests {
         buf.push(8);
         buf.push(9);
         assert_eq!(buf.into_iter().collect::<Vec<i32>>(), vec![8, 16, 8, 9]);
+    }
+
+    #[test]
+    fn index() {
+        let mut buf: OverwritingRingBuf<i32, 4> = OverwritingRingBuf::new();
+        buf.push(6);
+        buf.push(7);
+        buf.push(8);
+        buf.push(9);
+
+        buf[2] = 10;
+
+        assert_eq!(buf[0], 6);
+        assert_eq!(buf[1], 7);
+        assert_eq!(buf[2], 10);
+        assert_eq!(buf[3], 9);
+    }
+
+    #[test]
+    #[should_panic(expected = "assertion failed: index < self.length")]
+    fn out_of_bounds_index() {
+        let buf: OverwritingRingBuf<i32, 4> = OverwritingRingBuf::new();
+        let _ = buf[0];
     }
 }
